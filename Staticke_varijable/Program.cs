1a0414fe2581e69d651573cf80a5e4db15dd7a0c
Mannequin52 <92926451+Mannequin52@users.noreply.github.com>
2022-11-23 09:16:46 +0100

    Add files via upload

---
+++ Staticke_varijable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staticke_varijable
+{
+    class Program
+    {
+        class PrvaKlasa
+        {
+            private static int counter;
+
+            public static int Counter { get => counter; set => counter = value; }
+            public PrvaKlasa()
+            {
+                Counter++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PrvaKlasa x = new PrvaKlasa();
+            PrvaKlasa y = new PrvaKlasa();
+            PrvaKlasa z = new PrvaKlasa();
+
+            Console.WriteLine(PrvaKlasa.Counter);
+            Console.ReadKey();
+        }
+    }
+}
